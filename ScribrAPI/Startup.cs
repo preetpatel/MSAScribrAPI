2d1c608f8c9d2b58bebb49d0483a17a30dfd0ef6
Paul Tanchareon <paul.vtan@live.com>
2019-06-19 23:34:19 +1200

    Added Swagger to the project.

--- ScribrAPI/Startup.cs
+++ ScribrAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ScribrAPI
 {
@@ -26,11 +27,39 @@ namespace ScribrAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Scribr API",
+                    Version = "v1",
+                    Description = "A web API providing a custom toolkit for YouTube video transcription.",
+                    Contact = new Contact
+                    {
+                        Name = "MSA 2019 - Phase 2",
+                        Email = "nzmsa@microsoft.com",
+                        Url = "https://github.com/NZMSA/2018-Phase-2"
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ScribrAPI V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
