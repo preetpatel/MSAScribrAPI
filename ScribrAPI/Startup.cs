7e3b0b6d493dca6859d411b267d2412355eab702
David Lin <voidnullbot@outlook.com>
2019-06-20 23:26:07 +1200

    Removed redundant code

--- ScribrAPI/Startup.cs
+++ ScribrAPI/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ScribrAPI.Model;
-using ScribrAPI.Model;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace ScribrAPI
