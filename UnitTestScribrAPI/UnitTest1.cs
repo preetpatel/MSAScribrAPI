ecea9a6a23e6ca1e6b2aa7f9e27357a1683ffd7f
margeobur <margeobur@gmail.com>
2019-07-03 21:33:50 +1200

    added Unit Test project along with screenshots of the process

---
+++ UnitTestScribrAPI/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestScribrAPI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
